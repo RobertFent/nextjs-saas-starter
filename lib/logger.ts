0b090b5ec1b23fbe8db377f18d6541b1fe18cff7
Robert Fent <info@robertfent.com>
2025-10-17 21:27:34 +0200

    Disabled pino in vercel env

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,22 +1,19 @@
 import pino, { Logger } from 'pino';
 
+const isLocal = process.env.NODE_ENV !== 'production' && !process.env.VERCEL;
+
 /**
  * Defines a logger property that uses the pino library for logging with specific transport and level configurations based on the NODE_ENV environment variable.
- *
  * Usage: const log = logger.child({ side: 'server', 'server-action': 'users' });
- * log.debug('foo)
- *
- * @date Jul 10th 2025
- * @author Robot
- *
- * @type {Logger}
+ * log.debug('foo')
  */
-export const logger: Logger = pino({
-	transport: {
-		target: 'pino-pretty',
-		options: {
-			colorize: true
-		}
-	},
-	level: process.env['NODE_ENV'] === 'production' ? 'info' : 'debug'
-});
+export const logger: Logger = pino(
+	isLocal
+		? {
+				transport: {
+					target: 'pino-pretty',
+					options: { colorize: true }
+				}
+			}
+		: undefined
+);
