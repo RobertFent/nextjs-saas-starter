ac5f166db89480ade3a6cbd278325106c9563999
Robert Fent <info@robertfent.com>
2025-10-19 10:24:25 +0200

    Removed cypress snapshots

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'cypress';
 import { config } from 'dotenv';
-import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/plugin';
 // import { existsSync } from 'fs';
 import mockAPIServer from './cypress/support/mockAPISever';
 
@@ -14,9 +13,7 @@ config({ path: '.env' });
 
 export default defineConfig({
 	e2e: {
-		setupNodeEvents(on, config) {
-			addMatchImageSnapshotPlugin(on, config);
-
+		setupNodeEvents(on) {
 			// mockttp stuff: https://github.com/cypress-io/cypress/discussions/28469
 			on('before:run', () => {
 				mockAPIServer.start();
@@ -67,20 +64,6 @@ export default defineConfig({
 						`Unsupported browser: ${browser.name}. Only chromium is allowed at the moment.`
 					);
 				}
-				if (browser.isHeadless) {
-					// remove any existing --window-size or scale factor arguments before adding custom one to prevent duplicates
-					launchOptions.args = launchOptions.args.filter(
-						(arg) => {
-							return (
-								!arg.startsWith('--window-size') &&
-								!arg.startsWith(
-									'--force-device-scale-factor'
-								) &&
-								!arg.startsWith('--disable-web-resources')
-							);
-						} // also remove this for loading fonts in pipe to prevent snapshot problems
-					);
-				}
 
 				console.log('Browser launch options:', launchOptions.args);
 				return launchOptions;
@@ -89,11 +72,7 @@ export default defineConfig({
 		// this needs to be enabled for the mock server to work
 		experimentalInteractiveRunEvents: true,
 		specPattern: ['cypress/e2e/tests/**/*.cy.{ts,tsx}'],
-		// this is the dimension when running cypress headed
-		viewportWidth: 1920,
-		viewportHeight: 1080,
-		defaultCommandTimeout: 10000,
-		reporter: 'cypress-image-snapshot/reporter'
+		defaultCommandTimeout: 10000
 	},
 	supportFolder: 'cypress/support',
 	fixturesFolder: 'cypress/fixtures'
